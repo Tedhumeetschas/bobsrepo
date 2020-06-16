cdb06d72d33d9119cfca49c0d2be947a2651e2ba
tedhumeetschas <tedhu@pm.me>
2020-06-16 07:33:18 +0000

    new file:   myscript.sh 	new file:   using-echo-to-print 	modified:   sample.c

---
+++ myscript.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# ECHO COMMAND
+
+echo "Hello World!"
+
+# VARIABLES
+# Uppercase by convention
+# Letters, numbers, underscores
+
+NAME="Ted"
+# echo "My name is $(NAME)"
+echo "My name is ${NAME}"
+
