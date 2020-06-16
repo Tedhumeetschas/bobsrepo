c9708b2b083f6d87fe5061e72d407c9b5aa5c5bc
Tedhumeetschas <66975703+Tedhumeetschas@users.noreply.github.com>
2020-06-16 16:29:32 +0800

    Create cheat-sheet.sh

---
+++ cheat-sheet.sh
@@ -0,0 +1,52 @@
+#! bin/bash
+
+# ECHO COMMAND
+#echo "Hello World!"
+
+# VARIABLES
+# Upper case by convention
+# Letters, numbers, underscores
+#NAME="Ted"
+#echo "My name is $NAME"
+#echo "My name is ${NAME}"
+
+# USER INPUT
+#read -p "Enter your name: " NAME
+#echo "Hello $NAME, nice to meet you!"
+
+# SIMPLE IF STATEMENT
+#if [ "$NAME" == "Ted" ]
+#then
+#  echo "Your name is Ted"
+#fi
+
+#IF-ELSE
+
+#if [ "$NAME" == "Ted" ]
+#then
+#  echo "Your name is Ted"
+#else
+#  echo "Your name is not Ted"
+#fi
+
+#ELSE-IF (elif)
+
+if [ "$NAME" == "Ted" ]
+then
+  echo "Your name is Ted"
+elif [ "$NAME" == "Jack" ]  
+then
+  echo "Your name is not Ted or Jack"
+else
+  echo "Your name is not Ted"
+fi
+
+# COMPARISON
+
+
+# 
+#
+#
+#
+#
+#
